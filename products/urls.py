3eb439e9b861b98014ce159b6fa2c7445b70903d
Ayush  Shrestha <aayush625@gmail.com>
2025-11-27 16:13:54 +0545

    contact urls

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("products/", views.product_list, name="product_list"),
     path("products/<int:id>/", views.product_detail, name="product_detail"),
+  
 ]
