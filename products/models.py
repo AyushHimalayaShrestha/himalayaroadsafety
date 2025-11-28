a9dfc4457f1e890c27b193536e2093f0198a883d
Ayush  Shrestha <aayush625@gmail.com>
2025-11-28 10:35:40 +0545

    product image model

--- products/models.py
+++ products/models.py
@@ -84,6 +84,12 @@ class Inquiry(models.Model):
 
     def __str__(self):
         return f"Inquiry from {self.name}"
+    
+# Product Image
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
+    
 
 
 
--- safegearnepal/urls.py
+++ safegearnepal/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path("", include("products.urls")),
     path("contact/",include('contact.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
