8a546f338de5284ff5b232c432f4deba1fda69e1
Ayush  Shrestha <aayush625@gmail.com>
2025-12-03 12:23:33 +0545

    accounts app

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ayushshrestha/safegearnepal/.venv"
+set -gx VIRTUAL_ENV "/Users/ayushshrestha/himalayaroadsafety/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
