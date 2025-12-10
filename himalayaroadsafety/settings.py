37e44287af5566e4475fdfb6a41111c2c534d667
Ayush  Shrestha <aayush625@gmail.com>
2025-12-10 21:56:37 +0545

    .env for email configuration

--- himalayaroadsafety/settings.py
+++ himalayaroadsafety/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g(g_191er)dlju4ry9wo39v%+p$vc8g&8q4(z(b$ajhp+2z#9x"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-g(g_191er)dlju4ry9wo39v%+p$vc8g&8q4(z(b$ajhp+2z#9x")
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -145,7 +146,7 @@ LOGIN_URL = 'accounts:login'
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
